cbb0935e01d43bf568415619a768c49b9b11973b
Kristina <kmb250@miami.edu>
2018-05-09 10:16:24 -0400

    homework

--- Homework/p5/final/sketch.js
+++ Homework/p5/final/sketch.js
@@ -264,17 +264,17 @@ phase = frameCount * speed;
 var sizeOffset = (cos(phase)+ 1)* 0.5;
 var circleSize = sizeOffset * maxCircleSize;
 
-for( var strand = 0; strand < numStrands; strand +=1){
-  var strandPhase = phase +map(strand, 0, numStrands, 0, TWO_PI);
+  for( var strand = 0; strand < numStrands; strand +=1){
+    var strandPhase = phase +map(strand, 0, numStrands, 0, TWO_PI);
 
   for(var col = 0; col< numCols; col +=1){
     var colOffset = map(col, 0, numCols, 0, TWO_PI);
     var x = map(col, 0, numCols, 40, width - 100);
 
-    for( var row = 0; row < numRows; row +=1){
-      var y = height/2 + row * 10 + sin(strandPhase + colOffset) * 50;
-        var sizeOffset = (cos(strandPhase - (row / numRows) + colOffset) + 1) * 0.5;
-        var circleSize = sizeOffset * maxCircleSize;
+  for( var row = 0; row < numRows; row +=1){
+    var y = height/2 + row * 10 + sin(strandPhase + colOffset) * 50;
+    var sizeOffset = (cos(strandPhase - (row / numRows) + colOffset) + 1) * 0.5;
+    var circleSize = sizeOffset * maxCircleSize;
 
         fill(lerpColor(colorA, colorB, row / numRows));
         ellipse(x, y, circleSize, circleSize);
